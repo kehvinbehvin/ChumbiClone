0dde4c44406e2e59bebfa5a3c659e4205f4919a1
Kevin Ho <hokevin78@gmail.com>
2022-01-21 01:18:55 +0800

    feature: add chumbiclone contract

---
+++ contracts/ChumbiClone.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+pragma abicoder v2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "../utils/AccessProtected.sol";
+
+contract ChumbiClone is ERC721("ChumbiClone", "CHUMBICLONE"), AccessProtected {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    /**
+     * @notice - Mint NFT
+     * @dev - callable only by admin
+     *
+     * @param recipient - mint to
+     * @param tokenId - ID assigned to the token
+     * @param URI - uri of the land data
+     */
+    function mint(address recipient,uint256 tokenId, string memory URI)
+    external
+    onlyAdmin
+    {
+        _mint(recipient, tokenId);
+        _setTokenURI(tokenId, URI);
+    }
+
+    /**
+     * @notice - Set URI for token
+     * @dev - callable only by admin
+     *
+     * @param tokenId - Token ID of NFT
+     * @param tokenURI - URI to set
+     */
+    function setURI(uint256 tokenId, string memory tokenURI)
+    external
+    onlyAdmin
+    {
+        _setTokenURI(tokenId, tokenURI);
+    }
+
+    /**
+     * @notice - Set URI for token batch
+     * @dev - callable only by admin
+     *
+     * @param tokenIds - Token IDs of NFTs
+     * @param tokenURIs - URIs to set
+     */
+    function setBatchURI(uint256[] memory tokenIds, string[] memory tokenURIs)
+    external
+    onlyAdmin
+    {
+        require(tokenIds.length == tokenURIs.length, "Length mismatch");
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            _setTokenURI(tokenIds[i], tokenURIs[i]);
+        }
+    }
+}
